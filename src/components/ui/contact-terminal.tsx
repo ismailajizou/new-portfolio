04c5cb46feca5162d2f995ead26ab555941a4f3f
Ismail Ajizou <ismail.ajizou@arkx.group>
2024-06-18 18:34:48 +0100

    added focus state to the terminal and split the code to seperate hook

--- src/components/ui/contact-terminal.tsx
+++ src/components/ui/contact-terminal.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { RefObject, useCallback, useEffect, useRef, useState } from "react";
 import { z } from "zod";
-import { motion } from "framer-motion";
-type InferAsTuple = [unknown, ...unknown[]]
+type InferAsTuple = [unknown, ...unknown[]];
 const STEPS = [
   {
     prompt: "What is your name ?",
@@ -21,9 +20,9 @@ const STEPS = [
     validator: z.string().min(10).max(500),
   },
   {
-    prompt: "Wanna send it ?",
+    prompt: "Wanna send it ? (y/n)",
     input: "confirm",
-    validator: z.boolean(),
+    validator: z.string(),
   },
 ] satisfies InferAsTuple;
 
@@ -40,16 +39,121 @@ const LEVELS = {
   prompt: "text-gray-100",
 };
 
+const SPACE_CHAR = "\u00A0";
+
 type Fields = { [k in keyof typeof FIELDS]: string };
 type Line = {
   text: string;
   level: keyof typeof LEVELS;
 };
 
-const ContactTerminal = () => {
-  // this is a terminal like contact form
-  const [input, setInput] = useState<string>("");
+const useTyping = ({
+  handleNext,
+  terminalRef,
+}: {
+  handleNext: (input: string) => void;
+  terminalRef: RefObject<HTMLElement>;
+}) => {
+  const [focus, setFocus] = useState(false);
   const [cursor, setCursor] = useState<number>(0);
+  const [input, setInput] = useState<string>("");
+  const moveLeft = useCallback(() => {
+    if (cursor > 0) {
+      setCursor((prev) => prev - 1);
+    }
+  }, [cursor]);
+  const moveRight = useCallback(() => {
+    if (cursor < input.length) {
+      setCursor((prev) => prev + 1);
+    }
+  }, [cursor, input.length]);
+  const moveStart = useCallback(() => {
+    setCursor(0);
+  }, []);
+  const moveEnd = useCallback(() => {
+    setCursor(input.length);
+  }, [input.length]);
+  const resetInput = useCallback(() => {
+    setInput("");
+    setCursor(0);
+  }, [setInput]);
+  const handleTyping = useCallback(
+    (e: KeyboardEvent) => {
+      if (!focus) return;
+      e.preventDefault();
+      const key = e.key;
+
+      if (key === "Enter") {
+        handleNext(input);
+        resetInput();
+        return;
+      }
+      if (key === "Backspace") {
+        if (cursor > 0) {
+          setInput((prev) => prev.slice(0, cursor - 1) + prev.slice(cursor));
+          moveLeft();
+        }
+        return;
+      }
+      if (key === "ArrowLeft") {
+        if (cursor > 0) {
+          moveLeft();
+        }
+        return;
+      }
+      if (key === "ArrowRight") {
+        if (cursor < input.length) {
+          moveRight();
+        }
+        return;
+      }
+      if (key === "Home") {
+        setCursor(0);
+        return;
+      }
+      if (key === "End") {
+        setCursor(input.length);
+        return;
+      }
+      if (key.length === 1) {
+        setInput((prev) => prev.slice(0, cursor) + e.key + prev.slice(cursor));
+        setCursor((prev) => prev + e.key.length);
+      }
+    },
+    [cursor, handleNext, input, moveLeft, moveRight, resetInput],
+  );
+  useEffect(() => {
+    document.addEventListener("keydown", handleTyping);
+    return () => {
+      document.removeEventListener("keydown", handleTyping);
+    };
+  }, [handleTyping]);
+
+  useEffect(() => {
+    // manage focus inside the terminal
+    const handleClick = (e: MouseEvent) => {
+      if (terminalRef.current?.contains(e.target as Node)) {
+        setFocus(true);
+      } else {
+        setFocus(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return {
+    input,
+    cursor,
+    isFocused: focus,
+  };
+};
+
+const ContactTerminal = () => {
+  const terminalRef = useRef<HTMLDivElement>(null);
+
   const [lines, setLines] = useState<Line[]>([
     {
       text: STEPS[0].prompt,
@@ -62,15 +166,14 @@ const ContactTerminal = () => {
     email: "",
     message: "",
   });
-  const handleNext = useCallback(() => {
+
+  const handleNext = (input: string) => {
     if (step === STEPS.length - 1) {
-      // send the message
       console.log(fields);
       return;
     }
-    const valid = STEPS[step]!.validator.safeParse(
-      fields[Object.keys(FIELDS)[step] as keyof Fields],
-    );
+
+    const valid = STEPS[step]!.validator.safeParse(input.trim());
     if (!valid.success) {
       setFields({
         ...fields,
@@ -86,64 +189,47 @@ const ContactTerminal = () => {
       ]);
       return;
     }
+    if (step === STEPS.length - 2 && input.trim().toLowerCase() !== "y") {
+      setLines([
+        ...lines,
+        {
+          text: "Aborted.",
+          level: "info",
+        },
+      ]);
+      return;
+    }
+    if (step === STEPS.length - 2 && input.trim().toLowerCase() === "y") {
+      setLines([
+        ...lines,
+        {
+          text: "Sending message...",
+          level: "info",
+        },
+      ]);
+      return;
+    }
+    setFields({
+      ...fields,
+      [Object.keys(FIELDS)[step] as keyof Fields]: input.trim(),
+    });
     setLines([
       ...lines,
       {
-        text: fields[Object.keys(FIELDS)[step] as keyof Fields],
+        text: input,
         level: "info",
       },
       { text: STEPS[step + 1]!.prompt, level: "prompt" },
     ]);
     setStep(step + 1);
-  }, [step, fields, lines]);
-  const handleTyping = useCallback(
-    (e: KeyboardEvent) => {
-      console.log(e.key);
-      if (e.key === "Enter") {
-        handleNext();
-        return;
-      }
-      if (e.key === "Backspace") {
-        if (cursor > 0) {
-          setInput((prev) => prev.slice(0, cursor - 1) + prev.slice(cursor));
-          setCursor((prev) => prev - 1);
-        }
-        return;
-      }
-      if (e.key === "ArrowLeft") {
-        if (cursor > 0) {
-          setCursor((prev) => prev - 1);
-        }
-        return;
-      }
-      if (e.key === "ArrowRight") {
-        if (cursor < input.length) {
-          setCursor((prev) => prev + 1);
-        }
-        return;
-      }
-      if (e.key === "Home") {
-        setCursor(0);
-        return;
-      }
-      if (e.key === "End") {
-        setCursor(input.length);
-        return;
-      }
-      setInput((prev) => prev.slice(0, cursor) + e.key + prev.slice(cursor));
-      setCursor((prev) => prev + 1);
-    },
-    [cursor, handleNext, input],
-  );
-  useEffect(() => {
-    document.addEventListener("keydown", handleTyping);
-    return () => {
-      document.removeEventListener("keydown", handleTyping);
-    };
-  }, [handleTyping]);
+  };
+  const { input, cursor, isFocused } = useTyping({ handleNext, terminalRef });
 
   return (
-    <div className="mx-auto max-w-3xl overflow-hidden rounded-md border-2 border-card bg-card font-mono">
+    <div
+      ref={terminalRef}
+      className="mx-auto max-w-3xl overflow-hidden rounded-md border-2 border-card bg-card font-mono shadow-md"
+    >
       <TitleBar />
       <div className="h-96 overflow-y-scroll bg-gray-800/20 px-4 py-2 backdrop-blur-sm">
         <p>
@@ -161,23 +247,20 @@ const ContactTerminal = () => {
             </p>
           ))}
           <div className="flex w-full flex-wrap">
-            <p className="text-green-500">$ </p>
+            <p className="text-green-500">${SPACE_CHAR}</p>
             {input.split("").map((char, i) =>
-              i === cursor ? (
-                <span
-                  key={i}
-                  className="animate-caret duration-1 h-5 w-2.5 bg-green-500 text-gray-800"
-                >
-                   {char === " " ? "\u00A0" : char}
+              isFocused && i === cursor ? (
+                <span key={i} className=" bg-green-500 text-gray-800">
+                  {char === " " ? SPACE_CHAR : char}
                 </span>
               ) : (
                 <span key={i} className="text-gray-100">
-                   {char === " " ? "\u00A0" : char}
+                  {char === " " ? SPACE_CHAR : char}
                 </span>
               ),
             )}
-            {cursor === input.length && (
-              <span className="animate-caret duration-1 h-5 w-2.5 bg-green-500"/>
+            {isFocused && cursor === input.length && (
+              <span className="bg-green-500">{SPACE_CHAR}</span>
             )}
           </div>
         </div>
@@ -194,9 +277,7 @@ function TitleBar({ title }: { title?: string }) {
         <div className="h-3 w-3 rounded-full bg-yellow-500"></div>
         <div className="h-3 w-3 rounded-full bg-green-500"></div>
       </div>
-      <p className="justify-self-center">
-        {title ?? "Terminal"}
-      </p>
+      <p className="justify-self-center">{title ?? "Terminal"}</p>
     </div>
   );
 }
